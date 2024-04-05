784638bb2b78df8b7cd0141b39d4e778b529f166
iamtsj8888 <iamtsj8888@gmail.com>
2024-04-05 09:56:18 +0530

    Using resnet architechture

--- Model/train.py
+++ Model/train.py
@@ -30,6 +30,39 @@ def load_dataset(path, split=0.1):
 
     return (train_x, train_y), (valid_x, valid_y)
 
+def read_image(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = cv2.resize(x, (image_w, image_h))
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x
+
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x, (image_w, image_h))
+    x = x.astype(np.float32)    ## (h, w)
+    x = np.expand_dims(x, axis=-1)  ## (h, w, 1)
+    x = np.concatenate([x, x, x, x], axis=-1) ## (h, w, 4)
+    return x
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([image_h, image_w, 3])
+    y.set_shape([image_h, image_w, 4])
+    return x, y
+
+def tf_dataset(X, Y, batch=2):
+    ds = tf.data.Dataset.from_tensor_slices((X, Y))
+    ds = ds.map(tf_parse).batch(batch).prefetch(10)
+    return ds
+
 if __name__ == "__main__":
     """ Seeding """
     np.random.seed(42)
@@ -44,14 +77,39 @@ if __name__ == "__main__":
     input_shape = (image_h, image_w, 3)
     batch_size = 4
     lr = 1e-4
-    num_epochs = 100
+    num_epochs = 5
 
     """ Paths """
     dataset_path = "Model/people_segmentation"
-    model_path = os.path.join("Model/files", "model.h5")
+    model_path = os.path.join("Model/files", "model.keras")
     csv_path = os.path.join("Model/files", "data.csv")
 
     """ Loading the dataset """
     (train_x, train_y), (valid_x, valid_y) = load_dataset(dataset_path, split=0.2)
     print(f"Train: {len(train_x)}/{len(train_y)} - Valid: {len(valid_x)}/{len(valid_y)}")
     print("")
+
+    """ Dataset Pipeline """
+    train_ds = tf_dataset(train_x, train_y, batch=batch_size)
+    valid_ds = tf_dataset(valid_x, valid_y, batch=batch_size)
+
+    """ Model """
+    model = build_model(input_shape)
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=tf.keras.optimizers.Adam(lr)
+    )
+
+    """ Training """
+    callbacks = [
+        ModelCheckpoint(model_path, monitor='val_loss', verbose=1, save_best_only=True),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+        CSVLogger(csv_path, append=True),
+        EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=False)
+    ]
+
+    model.fit(train_ds,
+        validation_data=valid_ds,
+        epochs=num_epochs,
+        callbacks=callbacks
+    )

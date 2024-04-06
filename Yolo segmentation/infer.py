bd125eb3c59500429d85625c4ad32e4526833743
iamtsj8888 <iamtsj8888@gmail.com>
2024-04-06 23:44:56 +0530

    Segmentation data

--- Yolo segmentation/infer.py
+++ Yolo segmentation/infer.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import cv2
 import numpy as np
+from PIL import Image
 
 
 # Load a model
@@ -20,26 +21,18 @@ while True :
     masks_points = []
 
     annotated_img = frame
+    mask_img = frame
     for r in results:
         if(int(r.boxes.cls.item()) == 0):
             text = f"{text} \n----------- {r.boxes.cls.item()} -----------\n"
             text = text + str(r.masks) + "\n"
             masks_points = r.masks.xy
             annotated_img = r.plot(boxes = False, labels = False)
+            
+            mask_img = r.masks.data[0].cpu().numpy()
             break
-
-    # with open("masks.txt", "w")as file:
-    #     file.write(text)
-        
-    # annotated_img = frame
-    # if(len(masks_points) != 0):
-    #     masks_points.append(masks_points[0])
-    #     masks_points = np.array(masks_points, np.int32)
-    #     masks_points = masks_points.reshape((-1, 1, 2))
-    #     print(type(masks_points))
-    #     annotated_img = cv2.fillPoly(frame,masks_points,color=(0,0,255))
-
     
+    cv2.imshow("Mask", mask_img)    
     cv2.imshow("Segmentation", annotated_img)
 
     # Check if the user pressed the 'q' key
